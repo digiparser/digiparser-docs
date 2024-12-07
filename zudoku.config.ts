96911a6f9f98f0c4b496acc35cd48ba6aa8a265c
Pankaj Patidar <pankaj9296@gmail.com>
2024-12-07 14:05:57 +0530

    first commit

--- zudoku.config.ts
+++ zudoku.config.ts
@@ -1,6 +1,21 @@
 import type { ZudokuConfig } from "zudoku";
 
 const config: ZudokuConfig = {
+  metadata: {
+    title: "Digiparser Docs",
+    applicationName: "Digiparser Docs",
+    description: "Documentation for Digiparser",
+    logo: "https://www.digiparser.com/img/logo.svg",
+  },
+  page: {
+    pageTitle: "Digiparser Docs",
+    logo: {
+      src: {
+        light: "https://www.digiparser.com/img/logo.svg",
+        dark: "https://www.digiparser.com/img/logo.svg",
+      },
+    },
+  },
   topNavigation: [
     { id: "docs", label: "Documentation" },
     { id: "api", label: "API Reference" },
@@ -10,11 +25,16 @@ const config: ZudokuConfig = {
       {
         type: "category",
         label: "Overview",
-        items: ["introduction", "example"],
+        items: [
+          "/docs/introduction",
+        ],
       },
     ],
   },
-  redirects: [{ from: "/", to: "/docs/introduction" }],
+  redirects: [
+    { from: "/", to: "/docs/introduction" },
+    { from: "/docs", to: "/docs/introduction" },
+  ],
   apis: {
     type: "file",
     input: "./apis/openapi.yaml",
